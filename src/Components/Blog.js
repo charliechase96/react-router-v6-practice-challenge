a1881a3b3cfea7cee5ad6132f7ad5419e6bac2eb
Charlie Gray <charliechasegray@gmail.com>
2023-12-11 22:27:38 -0800

    fill out Blog and Post components, create db.json file

--- src/Components/Blog.js
+++ src/Components/Blog.js
@@ -1,6 +1,9 @@
-
+import { useState } from "react";
+import Post from "./Post";
 
 function Blog() {
+    const [posts, setPosts] = useState([]);
+    
     return (
         <p>Blog text</p>
     )
